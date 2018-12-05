de9604e5e1d78939f589a96af84d09d287e48464
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-12-05 19:17:50 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "select title, year from books where series_id = (SELECT MIN(id) FROM series);"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "select name, motto from characters where length(motto) = (select Max(length(motto)) from characters);"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select species, max(total) from (select species, count(*) total from characters group by species);"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "select a.name, s.name from authors a Join series on author_id = a.id join subgenres s on s.id = subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "select title from (select title, max(total) from (select title, count(c.author_id) total from series s left join characters c on c.author_id = s.author_id group by title));"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "select name, count(b.id) total from characters c left join character_books on character_id = c.id left join books b on b.id = book_id group by name order by total desc;"
 end
